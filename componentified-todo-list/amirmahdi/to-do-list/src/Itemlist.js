e96cb20b79c5bb6db85873812554dba0a7c80ae7
amirmahdibaharlou <amir.mbaharlou@yahoo.com>
2021-03-09 15:13:53 +0330

    Todo List with component

---
+++ componentified-todo-list/amirmahdi/to-do-list/src/Itemlist.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+
+const Itemlist = (props) => {
+    return(
+        <div>
+            <input type="checkbox" id={props.id} value={props.toDo} onChange={(e)=>props.done(e,props.id)}  />
+            <label className={props.complete ? 'done' : 'notdone'}>{props.toDo}</label> <br></br>
+            <button onClick={()=>props.top(props.id)}>Add to top</button>
+            <button onClick={()=>props.down(props.id)}>Add to bottom</button>
+            <button onClick={()=>props.deletetoDo(props.id)}>Remove</button>
+        </div>
+    )
+}
+
+export default Itemlist;
