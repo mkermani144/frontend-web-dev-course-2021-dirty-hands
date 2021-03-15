4d0df4d5176722fd8de8ed7a25fe0bf77bb7fcd3
Maryam Saeedmehr <6520696-BitterOcean@users.noreply.gitlab.com>
2021-03-15 16:25:05 +0330

    implemention has done

---
+++ todo-list-connected-to-api/maryam/src/AddNewItem.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import {
+  makeStyles,
+  ListItem,
+  IconButton,
+  ListItemSecondaryAction,
+  TextField
+} from '@material-ui/core';
+import {Add} from '@material-ui/icons'
+// ----------------------------------------------------------------
+const useStyles = makeStyles((theme) => ({
+  form: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+// ----------------------------------------------------------------
+const AddNewItem = () => {
+    const classes = useStyles();
+    const [textFieldValue, setTextFieldValue] = useState('');
+
+    const addClickedHandler = async () => {
+        fetch(`https://todolist.ehsan-rafee.ir/api/todolist`,{
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                title: textFieldValue,
+                ownerId: "5",
+                checked: false
+            })
+           })
+        .then(response => response.json())
+        .catch(error => console.error(error))
+    }
+    //-------------------------------------
+    return (
+        <ListItem dense>
+            <form className={classes.form} noValidate autoComplete="off">
+                <TextField
+                    fullWidth
+                    value={textFieldValue}
+                    onChange={e => setTextFieldValue(e.target.value)}
+                    edge="begin"
+                    id="standard-basic"
+                    label="Todo Item"
+                    required
+                />
+                <ListItemSecondaryAction>
+                <IconButton
+                    edge="end"
+                    onClick={addClickedHandler}
+                    aria-label="delete">
+                    <Add />
+                </IconButton>
+                </ListItemSecondaryAction>
+            </form>
+        </ListItem>
+    )
+}
+
+export default AddNewItem;
